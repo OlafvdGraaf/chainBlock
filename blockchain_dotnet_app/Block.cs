fca8d02e2c98126432cf654a2d96f9e78c1c1a42
OlafvdGraaf <0928885@hr.nl>
2018-05-16 16:00:03 +0200

    added block app
    dotnet publish
    docker build -t blockchainimage .
    docker run --name blockchaincontainer blockchainimage
    
    these 3 commands run the app, you must be inside the repository of the app (the Dockerfile is in there)

---
+++ blockchain_dotnet_app/Block.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace blockchain_dotnet_app
+{
+    class Block
+    {
+        /*hash of previous transactions which may not be changed*/
+        private int previousHash;
+        private String[] transactions;
+        private int hash;
+
+
+        public Block(String[] transactions, int previousHash)
+        {
+            this.transactions = transactions;
+            this.previousHash = previousHash;
+
+            this.updateBlock();
+        }
+
+        public int getHash()
+        {
+            return hash;
+        }
+
+        public void addTransaction(String transaction)
+        {
+            List<String> transList = this.transactions.ToList<String>();
+            transList.Add(transaction);
+            this.transactions = transList.ToArray<String>();
+
+            this.updateBlock();
+        }
+
+        private void updateBlock()
+        {
+            int[] combinedValues = { this.generateHash<String>(this.transactions), this.previousHash };
+            this.hash = this.generateHash<int>(combinedValues);
+            Console.WriteLine("A new Hash was generated for the Block: {0}", this.hash);
+        }
+
+        private int generateHash<T>(T[] transactions)
+        {
+            SHA256 sha256 = SHA256Managed.Create();
+
+            int result = 9;
+            foreach (var t in transactions)
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(t.ToString());
+                byte[] hash = sha256.ComputeHash(bytes);
+
+                result = result * BitConverter.ToInt32(hash, 0);
+            }
+            return result;
+        }
+    }
+}
---
+++ blockchain_dotnet_app/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace blockchain_dotnet_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            String[] transActions = {
+                "Diamant was won at Canada",
+                "Diamant arrived at Walmart",
+                "Diamant arrived in New York"
+            };
+
+            String[] transActions2 =
+            {
+                "nothing"
+            };
+
+            Block block1 = new Block(transActions, 0);
+
+            Block block2 = new Block(transActions, block1.getHash());
+
+            Block block3 = new Block(transActions, block2.getHash());
+
+            Block block4 = new Block(transActions, block3.getHash());
+
+            Block block5 = new Block(transActions, block4.getHash());
+
+            Block block6 = new Block(transActions, block5.getHash());
+            
+            Console.WriteLine("hello world");
+        }
+    }
+}
