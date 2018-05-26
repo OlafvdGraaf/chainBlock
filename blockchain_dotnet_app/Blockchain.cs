da130f9a4a5bf3cc52370cfc67cbc07de72635da
wouter <0934955@hr.nl>
2018-05-26 23:58:58 +0200

    Added simple proof of work / mining code

--- blockchain_dotnet_app/Blockchain.cs
+++ blockchain_dotnet_app/Blockchain.cs
@@ -21,6 +21,10 @@ namespace blockchain_dotnet_app
             this.chain = new List<Block>();
             this.current_transactions = new List<Transaction>();
             this.last_block = chain.Last();
+
+            //create genisis block
+            this.newBlock(100, "none");
+      
         }
 
         public Block newBlock(int proof, string previous_hash)
@@ -52,15 +56,47 @@ namespace blockchain_dotnet_app
         public static string hash(Block block)
         {
             SHA256 sha256 = SHA256Managed.Create();
-            SHA256 test = SHA256Managed.Create();
 
             byte[] bytes = Encoding.UTF8.GetBytes(Newtonsoft.Json.JsonConvert.SerializeObject(block));
 
             byte[] hash = sha256.ComputeHash(bytes);
 
-            string result = BitConverter.ToString(hash, 0);
+            string result = BitConverter.ToString(hash,0);
 
             return result;
+        }
+
+        public static bool validProof(int last_proof, int proof)
+        {
+            //create new sha256 object
+            SHA256 sha256 = SHA256Managed.Create();
+
+            //xor the last proof against a new proof
+            int guess = last_proof ^ proof;
+
+            // encode the xor to bytes
+            byte[] bytes = Encoding.UTF8.GetBytes(guess.ToString());
+
+            // get a hash from the encoded bytes
+            byte[] hash = sha256.ComputeHash(bytes);
+
+            // convert the hash to a hexadecimal string
+            string result = BitConverter.ToString(hash, 0);
+
+            //check if the first 4 characters of the hexadecimal hash are 0000
+            return result.Substring(0, 4) == "0000";
+
+        }
+
+        public int proofOfWork(int last_proof)
+        {
+            int proof = 0;
+            
+            while(!validProof(last_proof, proof){
+                proof++;
+            }
+
+            return proof;
         }
     }
 }
