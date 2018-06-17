489d68b6bed593116435d3cd63d5014335e30561
OlafvdGraaf <0928885@hr.nl>
2018-06-17 23:00:25 +0200

    Added client to project

---
+++ blockchain_dotnet_client/Client.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace blockchain_dotnet_client
+{
+    class Client
+    {
+
+        public string Get(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        public string Post(string uri, string data, string contentType, string method = "POST")
+        {
+            byte[] dataBytes = Encoding.UTF8.GetBytes(data);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            request.ContentLength = dataBytes.Length;
+            request.ContentType = contentType;
+            request.Method = method;
+
+            using (Stream requestBody = request.GetRequestStream())
+            {
+                requestBody.Write(dataBytes, 0, dataBytes.Length);
+            }
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
---
+++ blockchain_dotnet_client/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace blockchain_dotnet_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client chainClient = new Client();
+
+            string command = "";
+
+            while (command != "exit")
+            {
+                Console.WriteLine("Waiting for a new command; \n'exit = exit' \n'getbc = get blockchain' \n'postdm = post diamond to blockchain' \n");
+                command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case "getbc":
+                        Console.Clear();
+                        Console.WriteLine("\nHTTP server response:");
+                        Console.WriteLine(chainClient.Get("http://localhost:5000/Blockchain/chain") + "\n");
+                        Console.WriteLine("press any key to continue...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+                    case "postdm":
+                        Console.Clear();
+                        Console.WriteLine("\nHTTP server response:");
+                        //Console.WriteLine(chainClient.Post("http://localhost:5000/Blockchain/chain") + "\n");
+                        Console.WriteLine("Body is yeat to be created...");
+                        Console.WriteLine("press any key to continue...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                }
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
